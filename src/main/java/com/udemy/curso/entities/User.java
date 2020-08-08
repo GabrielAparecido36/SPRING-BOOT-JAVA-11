ad100c3107ee5af6fab7a52146995809546d24e9
Gabriel Aparecido <contato.gabrielaparecido36@gmail.com>
2020-08-08 02:26:04 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/udemy/curso/entities/User.java
+++ src/main/java/com/udemy/curso/entities/User.java
@@ -13,7 +13,7 @@ public class User implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
@@ -23,7 +23,7 @@ public class User implements Serializable {
 	
 	}
 
-	public User(long id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -32,7 +32,7 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
